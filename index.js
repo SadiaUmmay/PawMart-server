327a948b9021e854bd09c0eb6548aecf3c07172c
unknown <ummayjannatsadia@gmail.com>
2025-12-05 22:30:45 +0600

    service post

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://petpaw:e1KrIAQHTyPCOer9@cluster0.yfrfj.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfrfj.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,94 +26,14 @@ async function run() {
     
 
     
+
     app.post("/service", async (req, res) => {
       const service = req.body;
       const result = await serviceCollection.insertOne(service);
       res.send({ success: true, insertedId: result.insertedId });
     });
 
-    app.get("/service", async (req, res) => {
-      const result = await serviceCollection.find({}).toArray();
-      res.send(result);
-    });
-
-   app.get("/service/:id", async (req, res) => {
-      const id = req.params.id;
-      
-      const service = await serviceCollection.findOne({ _id: new ObjectId(id) });
-      res.send(service);
-    });
-
-
-    app.post('/orders',async(req,res)=>{
-  const data=req.body
-  const result=await ordercollection.insertOne(data)
-  res.status(201).send(result)
-  console.log(result);
-  
-
-})
-
-app.get("/orders", async (req, res) => {
-  try {
-    const result = await ordercollection.find({}).toArray();
-    res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Something went wrong" });
-  }
-});
-
-    app.get("/mylistings", async (req, res) => {
-      const { email } = req.query;
-      const query = email ? { email: email } : {};
-      const result = await serviceCollection.find(query).toArray();
-      res.send(result);
-    });
-
-
-    // UPDATE service (PUT)
-app.put("/mylistings/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const updatedService = req.body;
-
-    const result = await serviceCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: updatedService }
-    );
-
-    if (result.modifiedCount === 0) {
-      return res.status(404).send({ message: "Service not found!" });
-    }
-
-    res.send({ success: true, message: "Service updated successfully!" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Failed to update service" });
-  }
-});
-
-// DELETE service 
-app.delete("/mylistings/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const result = await serviceCollection.deleteOne({
-      _id: new ObjectId(id),
-    });
-
-    if (result.deletedCount === 0) {
-      return res.status(404).send({ message: "Service not found!" });
-    }
-
-    res.send({ success: true, message: "Service deleted successfully!" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Failed to delete service" });
-  }
-});
-
+   
 
 
     // Test ping
